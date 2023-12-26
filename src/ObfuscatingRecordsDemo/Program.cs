34bf493aa5fb0da263a6568146c89ae36f6edf5b
Farkhod <farkhod.dadajanov@consolsys.com>
2023-12-26 09:26:48 +0800

    initial commit

---
+++ src/ObfuscatingRecordsDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿Circle circle = new (5);
+Console.WriteLine($"Area of Circle: {circle.CalculateArea()}");
+
+Rectangle rectangle = new (4, 6);
+Console.WriteLine($"Area of Rectangle: {rectangle.CalculateArea()}");
+
+Triangle triangle = new (3, 8);
+Console.WriteLine($"Area of Triangle: {triangle.CalculateArea()}");
+
+Console.ReadLine();
---
+++ src/ObfuscatingRecordsDemo/Shape.cs
@@ -0,0 +1,28 @@
+﻿public abstract record Shape
+{
+    public abstract double CalculateArea();
+}
+
+public record Circle(double Radius) : Shape
+{
+    public override double CalculateArea()
+    {
+        return Math.PI * Math.Pow(Radius, 2);
+    }
+}
+
+public record Rectangle(double Length, double Width) : Shape
+{
+    public override double CalculateArea()
+    {
+        return Length * Width;
+    }
+}
+
+public record Triangle(double BaseLength, double Height) : Shape
+{
+    public override double CalculateArea()
+    {
+        return 0.5 * BaseLength * Height;
+    }
+}
